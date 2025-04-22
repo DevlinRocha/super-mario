4e097d678a6bb679037863b06c3b93c42a88aa6c
Devlin <devlin@devlinrocha.com>
2025-04-22 15:17:54 -0700

    create initial block hitting logic

--- scripts/block.gd
+++ scripts/block.gd
@@ -1,8 +1,14 @@
 class_name Block
 
+
 extends StaticBody2D
 
+
 signal hit
+
+
+@export var item: PackedScene
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,4 +16,6 @@ func _ready() -> void:
 
 
 func _on_hit() -> void:
-	queue_free()
+	var item_instance := item.instantiate()
+	add_sibling(item_instance)
+	item_instance.position = position + Vector2(0, -16)
