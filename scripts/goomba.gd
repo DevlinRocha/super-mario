580078ca0489fd6daf401620392e3bbb00c7c32e
Devlin <devlin@devlinrocha.com>
2025-04-30 16:11:31 -0700

    create death sequence

--- scripts/goomba.gd
+++ scripts/goomba.gd
@@ -10,11 +10,14 @@ const SPEED := 40
 var direction := -1
 
 
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var hurtbox: Area2D = $Hurtbox
+@onready var hitbox: Area2D = $Hitbox
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _ready() -> void:
-	hit.connect(_on_hit)
+	hurtbox.area_entered.connect(_on_area_entered_hurtbox)
 
 
 func _physics_process(delta: float) -> void:
@@ -23,15 +26,15 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	velocity.x = direction * SPEED
-	animation_player.play("Moving")
+	if direction != 0:
+		animation_player.play("Moving")
 
 	move_and_slide()
 
-	for i in get_slide_collision_count():
-		var collider = get_slide_collision(i).get_collider()
-		if collider is Player:
-			collider.hit.emit()
 
-
-func _on_hit() -> void:
-	queue_free()
+func _on_area_entered_hurtbox(area: Area2D) -> void:
+	animation_player.stop()
+	direction = 0
+	sprite_2d.frame = 2
+	hit.emit()
+	await get_tree().create_timer(0.25).timeout.connect(func(): queue_free())
