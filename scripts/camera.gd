a6546dea947333ca7bc73c15586ae28cf899b27a
Devlin <devlin@devlinrocha.com>
2025-05-01 03:53:48 -0700

    dynamically set left limit as player moves right

---
+++ scripts/camera.gd
@@ -0,0 +1,18 @@
+extends Camera2D
+
+
+var starting_position: float
+var greatest_position: float
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	starting_position = get_screen_center_position().x
+	greatest_position = starting_position
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var current_position = get_screen_center_position().x
+	if current_position > greatest_position:
+		greatest_position = current_position
+		limit_left = greatest_position - starting_position
