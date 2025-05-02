8304e71842daefc1e8552d814442fe10a2e0e2af
Devlin <devlin@devlinrocha.com>
2025-05-02 00:46:09 -0700

    decrement timer every second

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -4,13 +4,14 @@ extends CanvasLayer
 @onready var player: Player = $"../Player"
 @onready var score: Label = %Score
 @onready var coin_count: Label = %CoinCount
+@onready var timer: Label = $HUD/Time/Timer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player.coin_collected.connect(_on_coin_collected)
 	player.increase_score.connect(increase_score)
-
+	decrement_timer()
 
 func _on_coin_collected() -> void:
 	increase_score(200)
@@ -23,3 +24,9 @@ func increase_score(value: int) -> void:
 
 func increase_coins(value: int) -> void:
 	coin_count.text = "x" + str("%02d" % (int(coin_count.text) + value))
+
+
+func decrement_timer() -> void:
+	timer.text = str(int(timer.text) - 1)
+	if int(timer.text) > 0:
+		get_tree().create_timer(1).timeout.connect(decrement_timer)
