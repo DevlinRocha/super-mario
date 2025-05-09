c6f8914cc058116836f6f9a5719675129e8308c8
Devlin <devlin@devlinrocha.com>
2025-05-09 12:46:22 -0700

    create initial collection logic

--- scripts/mushroom.gd
+++ scripts/mushroom.gd
@@ -6,6 +6,13 @@ var in_block := false
 var direction := 1
 
 
+@onready var area_2d: Area2D = $Area2D
+
+
+func _ready() -> void:
+	area_2d.body_entered.connect(_on_body_entered)
+
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor() and not in_block:
@@ -14,3 +21,9 @@ func _physics_process(delta: float) -> void:
 	if not in_block:
 		velocity.x = direction * SPEED
 		move_and_slide()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		queue_free()
+		body.mushroom_collected.emit()
