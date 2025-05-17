e1e5c7efa5139f4f04c63ddeca80a22704de2385
Devlin <devlin@devlinrocha.com>
2025-05-17 12:44:53 -0700

    koopa spawns a shell when hit

--- scripts/koopa.gd
+++ scripts/koopa.gd
@@ -1,11 +1,12 @@
 extends Enemy
 
 
+const SHELL = preload("res://scenes/shell.tscn")
+
+
 func _on_area_entered_hurtbox(area: Area2D) -> void:
 	if area.is_in_group("Hitbox"):
-		animation_player.stop()
-		direction = 0
-		hurtbox.collision_layer = 0
-		hurtbox.collision_mask = 0
-		hitbox.collision_layer = 0
-		await get_tree().create_timer(0.5, false).timeout.connect(func() -> void: queue_free())
+		var shell := SHELL.instantiate()
+		shell.position = position
+		add_sibling(shell)
+		queue_free()
