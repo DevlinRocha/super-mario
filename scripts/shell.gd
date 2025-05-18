0f2dee4393d6e0bcf838ce319c65f92e5d28b3d0
Devlin <devlin@devlinrocha.com>
2025-05-18 13:31:27 -0700

    improve player interactions with shell

--- scripts/shell.gd
+++ scripts/shell.gd
@@ -8,12 +8,15 @@ const SPEED := 160
 var direction := 0
 
 
-@onready var area_2d: Area2D = $Area2D
+@onready var hurtbox: Area2D = $Hurtbox
+@onready var hitbox: Area2D = $Hitbox
 
 
 func _ready() -> void:
-	area_2d.body_entered.connect(_on_body_entered_area_2d)
+	hurtbox.area_entered.connect(_on_area_entered_hurtbox)
+	hurtbox.body_entered.connect(_on_body_entered_hurtbox)
 	hit.connect(_on_hit)
+	hitbox.collision_layer = 0
 
 
 func _physics_process(delta: float) -> void:
@@ -30,17 +33,31 @@ func _physics_process(delta: float) -> void:
 		direction *= -1
 
 
-func _on_body_entered_area_2d(body: Node2D) -> void:
-	if body is Player:
+func _on_area_entered_hurtbox(area: Area2D) -> void:
+	if area.is_in_group("Hitbox"):
 		if direction == 0:
-			if body.sprite_2d.flip_h:
+			if area.get_parent().sprite_2d.flip_h:
 				direction = -1
 			else:
 				direction = 1
+			hitbox.collision_layer = 2
 		else:
-			body.hit.emit()
+			direction = 0
+			hitbox.collision_layer = 0
+
+
+func _on_body_entered_hurtbox(body: Node2D) -> void:
+	if body is Player and body.is_on_floor() and direction == 0:
+		if body.sprite_2d.flip_h:
+			direction = -1
+		else:
+			direction = 1
+		hitbox.collision_layer = 2
 
 
 func _on_hit() -> void:
+	hurtbox.collision_layer = 0
+	hurtbox.collision_mask = 0
+	hitbox.collision_layer = 0
 	collision_mask = 0
 	velocity.y = -160
